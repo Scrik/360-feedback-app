d3825be08b54bd23055caf2e91434528a7ebbae1
MitchBred <mail@mitchellbreden.nl>
2020-02-26 21:09:20 +0100

    updated expo to version 36

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -176,7 +176,6 @@ export default class LoginScreen extends React.Component {
                                 </View>
                             </View>
                         </View>
-
                     }
                 />
             </View>
